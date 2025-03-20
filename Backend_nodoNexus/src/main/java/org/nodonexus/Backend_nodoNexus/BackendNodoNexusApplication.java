889a14347929fe206c43fbadf43cf417e336d5c0
JDAAR <jdanaconar@gmail.com>
2025-03-20 14:39:58 -0500

    Empezando proyecto para nodo nexus asignando credenciales para el repositorio se crearan la rama main que es para produccion la rama develop que es para desarrollo y dentro de esta se van a crear las ramas dependiendo de las funcionalidades o la implementacion

---
+++ Backend_nodoNexus/src/main/java/org/nodonexus/Backend_nodoNexus/BackendNodoNexusApplication.java
@@ -0,0 +1,13 @@
+package org.nodonexus.Backend_nodoNexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNodoNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNodoNexusApplication.class, args);
+	}
+
+}
---
+++ Backend_nodoNexus/src/test/java/org/nodonexus/Backend_nodoNexus/BackendNodoNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nodonexus.Backend_nodoNexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNodoNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
